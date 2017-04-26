a3b20de8668f98ce064f2ec9be80edfe663eb66e
mohamed <mohamdib@gmail.com>
2017-04-26 12:21:55 +0300

    validations

--- models/attraction.js
+++ models/attraction.js
@@ -13,7 +13,7 @@ var attSchema = new Schema({
   city: String,
   phone: Number,
   area: String,
-  time: String,
+  time: Number,
   details: String,
   timerating: Number,
   engoyrating: Number,
